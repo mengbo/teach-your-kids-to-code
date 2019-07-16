c10c6c70e4bb1409e23fb4dae4d55e3d10968d31
Meng Bo <mengbo@lnu.edu.cn>
2019-07-16 12:15:42 +0800

    use pipenv.

--- initenv.sh
+++ initenv.sh
@@ -1,11 +1,5 @@
 #!/bin/sh -v
 
-PY_VERSION=3.6.0
-
-pyenv local --unset
-pyenv uninstall teach-your-kids-to-code
-pyenv virtualenv $PY_VERSION teach-your-kids-to-code
-pyenv local teach-your-kids-to-code
-
-pip install pygame
-pip install pylint
+pipenv --python 3
+pipenv install pygame
+pipenv install --dev pylint
